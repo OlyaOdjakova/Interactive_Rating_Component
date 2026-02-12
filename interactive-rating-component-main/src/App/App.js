1cc774c48a24c875e3c6b06480d3bca0f894e771
OlyaOdjakova <Olgica.Odzakova@students.finki.ukim.mk>
2026-02-12 02:37:51 +0100

    Add files

---
+++ interactive-rating-component-main/src/App/App.js
@@ -0,0 +1,17 @@
+import './App.module.css';
+import Card from "../Card/card";
+import styles from "./App.module.css";
+
+function App() {
+    return (
+        <>
+            <div className={styles.background}>
+                <Card/>
+            </div>
+        </>
+    );
+}
+
+export default App;
+
+
